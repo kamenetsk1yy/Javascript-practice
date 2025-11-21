acf72639cc7981b03f882b2a16e8c5cce949d530
mer1ED <167485504+mer1ED@users.noreply.github.com>
2025-11-21 06:51:31 +0200

    Functions HW

--- script.js
+++ script.js
@@ -1,102 +1,40 @@
-//1
-// let a = prompt();
+// 1
 
-// switch (true) {
-//   case a > 0:
-//     console.log(true);
-//     break;
-
-//   case a < 0:
-//     console.log(false);
-//     break;
+// function checkAge(age) {
+//    const allow = age >= 18 ? "совершеннолетний" : "несовершеннолетний";
+//    return console.log(allow);
 // }
+
+// checkAge(44);
 
 //2
-// let a = prompt();
 
-// switch (true) {
-//   case a == "test":
-//     console.log(true);
-//     break;
-//   case a !== "test":
-//     console.log(false);
-//     break;
+// const min = (a, b) => {
+//   if (a < b) {
+//     console.log(a);
+//     return a;
+//   } else {
+//     console.log(b);
+//     return b;
+//   }
 // }
+
+// min(10, 2);
 
 //3
-// let a = Number(prompt());
-
-// switch (true) {
-//   case a >= 10:
-//     console.log(a - 5);
-//     break;
-//   case a < 10:
-//     console.log(a + 5);
-//     break;
+// function showOk() {
+//   console.log("ви погодились");
 // }
 
-// 4;
-// const value = prompt("Введіть порядкування місяців").toLowerCase();
-// let month;
-
-// switch (value) {
-//   case "1":
-//     month = "Січень";
-//     alert(`${value}-й місяць це ${month}`);
-//     break;
-//   case "2":
-//     month = "Лютий";
-//     alert(`${value}-й місяць це ${month}`);
-//     break;
-//   case "3":
-//     month = "Березень";
-//     alert(`${value}-й місяць це ${month}`);
-//     break;
-//   case "4":
-//     month = "Квітень";
-//     alert(`${value}-й місяць це ${month}`);
-//     break;
-//   case "5":
-//     month = "Травень";
-//     alert(`${value}-й місяць це ${month}`);
-//     break;
-//   case "6":
-//     month = "Червень";
-//     alert(`${value}-й місяць це ${month}`);
-//     break;
-//   case "7":
-//     month = "Липень";
-//     alert(`${value}-й місяць це ${month}`);
-//     break;
-//   case "8":
-//     month = "Серпень";
-//     alert(`${value}-й місяць це ${month}`);
-//     break;
-//   case "9":
-//     month = "Вересень";
-//     alert(`${value}-й місяць це ${month}`);
-//     break;
-//   case "10":
-//     month = "Жовтень";
-//     alert(`${value}-й місяць це ${month}`);
-//     break;
-//   case "11":
-//     month = "Листопад";
-//     alert(`${value}-й місяць це ${month}`);
-//     break;
-//   case "12":
-//     month = "Грудень";
-//     alert(`${value}-й місяць це ${month}`);
-//     break;
-
-//   default:
-//     if (value > 12) {
-//       alert("дурачок?");
-//     }
+// function showCancel() {
+//   console.log("ви непогодились");
 // }
 
-// 5
-// const value = prompt("Введіть тризначне число");
-// let sum = Number(value[0]) + Number(value[1]) + Number(value[2]);
-
-// alert(`Сума цифр = ${sum}`);
+// const ask = (question, yes, no) => {
+//   if (confirm(question)) {
+//     yes();
+//   } else {
+//     no();
+//   }
+// };
+// ask("Ви погоджуєтесь?", showOk, showCancel);
